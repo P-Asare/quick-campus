9028fc2c7f95e66ca48454cf8e706bd9dcdde849
Palal <86783671+P-Asare@users.noreply.github.com>
2024-07-18 14:15:25 +0000

    Backnd login and sign up (#9)
    * add model class
    
    * add user model
    
    * add user controller class
    
    * remove file excecption import
    
    * add validation middleware
    
    * add login and register endpoints
    
    * add routes
    
    * add role id to request
    
    * add role to login response

---
+++ backend/app/models/user.php
@@ -0,0 +1,78 @@
+<?php
+require_once __DIR__ . '/model.php';
+
+/// Class to represent the Users database
+class User extends Model
+{
+    protected $table = 'users';
+
+    // Create single user
+    public function createUser($firstname, $lastname, $email, $password, $role)
+    {
+        $password_hash = password_hash($password, PASSWORD_DEFAULT);
+        
+        $data = [
+            'firstName' => $firstname,
+            'lastName' => $lastname,
+            'email' => $email,
+            'password' => $password_hash,
+            'role_id' => $role
+        ];
+
+        return $this->insert($data);
+    }
+
+    // Find a user by their email address
+    public function findByEmail($email)
+    {
+        $result = $this->find("email", $email);
+        return $result;
+    }
+
+    // Fetch all the users in the system with a specific set of column details
+    public function fetchAll()
+    {
+        $sql = "SELECT user_id, firstName, lastName, email, profile_image
+                FROM {$this->table}";
+
+        $stmt = $this->pdo->query($sql);
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    // Fetch all drivers in the system with a specific set of column details (attributes)
+    public function fetchAllDrivers()
+    {
+        $sql = "SELECT user_id, firstName, lastName, email, profile_image
+                FROM {$this->table} WHERE role_id = 3";
+
+        $stmt = $this->pdo->query($sql);
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    // Find a user by their id
+    public function findProfileById($id)
+    {
+        $sql = "SELECT user_id, firstname, lastname, email, role_id, profile_image FROM " . $this->table . " WHERE user_id = :id";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute(['id' => $id]);
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    // Update user profile image
+    public function updateProfileImage($id, $imagePath){
+        $sql = "UPDATE {$this->table} SET profile_Image = :profile_Image WHERE userId = :id";
+        $stmt = $this->pdo->prepare($sql);
+        return $stmt->execute(['profile_Image' => $imagePath, 'id' => $id]);
+    }
+
+    // update password
+    public function resetPassword($email, $newPassword)
+    {
+        $password_hash = password_hash($newPassword, PASSWORD_DEFAULT);
+        $sql = "UPDATE {$this->table} SET password = :password WHERE email = :email";
+        $stmt = $this->pdo->prepare($sql);
+        return $stmt->execute(['password' => $password_hash, 'email' => $email]);
+    }
+
+}
+?>
--- backend/index.php
+++ backend/index.php
@@ -1,17 +1,72 @@
 <?php
 
+    header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, PATCH, OPTIONS');
+    header('Access-Control-Max-Age: 1000');
+    header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
+    header('content-Type: application/json');
+
     require_once __DIR__ . '/vendor/autoload.php';
     require_once __DIR__ . '/config/database.php';
+    require_once __DIR__ . '/app/middleware/validationMiddleWare.php';
+    require_once __DIR__ . '/app/controllers/userController.php';
 
     use Dotenv\Dotenv;
 
     $dotenv = Dotenv::createImmutable(__DIR__);
     $dotenv->load();
 
+    $router = new AltoRouter();
+    $router->setBasePath('/quick-campus/backend');
+
     // create database and pdo
     $database = new Database();
     $pdo = $database->getPdo();
 
-    echo "Working";
+    $userController = new UserController($pdo);
+
+    // Routes
+    // Below I will define all the different end points clients can interact with
+
+    // cater for user register
+    $router->map('POST', '/users', function () use ($userController) {
+
+        $data = json_decode(file_get_contents('php://input'), true);
+
+        // validate data
+        ValidationMiddleWare::handle($data, [
+            'firstname' => 'string',
+            'lastname' => 'string',
+            'email' => 'email',
+            'password' => 'password',
+            'confirm_password' => 'confirm_password',
+            'role' => 'integer'
+        ]);
+
+        echo json_encode($userController->createUser($data));
+    });
+
+    // Cater for user login
+    $router->map('POST', '/users/login', function () use ($userController) {
+
+        $data = json_decode(file_get_contents('php://input'), true);
+
+        // validate data
+        ValidationMiddleWare::handle($data, [
+            'email' => 'string',
+            'password' => 'password',
+        ]);
+
+        echo json_encode($userController->login($data));
+    });
     
+    // Match the request to a defined endpoint
+    $match = $router->match();
+
+    if ($match && is_callable($match['target'])) {
+        call_user_func_array($match['target'], $match['params']);
+    } else {
+        // No route was matched
+        http_response_code(404);
+        echo json_encode(['status' => 'error', 'message' => 'Route not found']);
+    }
 ?>
