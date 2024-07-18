e7aa569680f221944db78f36527d74fe19d28881
P-Asare <pinopalal11@gmail.com>
2024-07-18 17:31:23 +0000

    add: request controller and model

---
+++ backend/app/controllers/requestController.php
@@ -0,0 +1,45 @@
+<?php
+
+    require_once __DIR__ . '/../models/request.php';
+
+    /// Class to cater for all request oriented actions
+    class RequestController{
+
+        protected $requestModel;
+
+        public function __construct($pdo){
+            $this->requestModel = new Request($pdo);
+        }
+        
+        // create a delivery request
+        public function placeRequest($data){
+
+            try {
+                $this->requestModel->createRequest($data);
+
+                return ['success' => true];
+            } catch (\Exception $e) {
+                header('HTTP/1.1 422 Unprocessable Entity');
+                $errors = ["success" => false, "error" => $e->getMessage()];
+                return $errors;
+            }      
+        }
+
+        // get orders by user
+        public function getRequestsByUser($column, $user_id){
+
+            try {
+                
+                $requests = $this->requestModel->findRequestsByUser($column, $user_id);
+                return ['success' => true, 'data' => $requests];
+
+            } catch (\Exception $e) {
+                header('HTTP/1.1 422 Unprocessable Entity');
+                $errors = ["success" => false, "error" => $e->getMessage()];
+                return $errors;
+            }
+        }
+
+
+    }
+?>
---
+++ backend/app/models/request.php
@@ -0,0 +1,33 @@
+<?php
+
+    require_once __DIR__ . '/model.php';
+
+    /// Class to represent a delivery/pickup request
+    class Request extends Model {
+
+        protected $table = 'requests';
+
+        // Create a request
+        public function createRequest($data){
+
+            // put data in table
+            return $this->insert($data);
+        }
+
+        // Find request by Id
+        public function findRequestById($request_id){
+
+            return $this->find('request_id', $request_id);
+        }
+
+        // Fetch all requests for a user
+        public function findRequestsByUser($column, $user_id){
+
+            $sql = "SELECT * FROM {$this->table} WHERE $column = :user_id";
+            $stmt = $this->pdo->prepare($sql);
+            $stmt->execute(['user_id' => $user_id]);
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        }
+    }
+
+?>
