bad7ce5d799cef63e430fa87eb16ef3e80adc774
P-Asare <pinopalal11@gmail.com>
2024-07-18 13:30:56 +0000

    add login and register endpoints

--- backend/index.php
+++ backend/index.php
@@ -1,16 +1,62 @@
 <?php
 
+    header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, PATCH, OPTIONS');
+    header('Access-Control-Max-Age: 1000');
+    header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
+    header('content-Type: application/json');
+
     require_once __DIR__ . '/vendor/autoload.php';
     require_once __DIR__ . '/config/database.php';
+    require_once __DIR__ . '/app/middleware/validationMiddleWare.php';
+    require_once __DIR__ . '/app/controllers/userController.php';
 
     use Dotenv\Dotenv;
 
     $dotenv = Dotenv::createImmutable(__DIR__);
     $dotenv->load();
 
+    $router = new AltoRouter();
+    $router->setBasePath('/quick-campus/backend');
+
     // create database and pdo
     $database = new Database();
     $pdo = $database->getPdo();
+
+    $userController = new UserController($pdo);
+
+    // Routes
+    // Below I will define all the different end points clients can interact with
+
+    // cater for user register
+    $router->map('POST', '/users', function () use ($userController) {
+
+        $data = json_decode(file_get_contents('php://input'), true);
+
+        // validate data
+        ValidationMiddleWare::handle($data, [
+            'firstname' => 'string',
+            'lastname' => 'string',
+            'email' => 'email',
+            'password' => 'password',
+            'confirm_password' => 'confirm_password',
+        ]);
+
+        echo json_encode($userController->createUser($data));
+    });
+
+    // Cater for user login
+    $router->map('POST', '/users/login', function () use ($userController) {
+
+        $data = json_decode(file_get_contents('php://input'), true);
+
+        // validate data
+        ValidationMiddleWare::handle($data, [
+            'email' => 'string',
+            'password' => 'password',
+        ]);
+
+        echo json_encode($userController->login($data));
+    });
 
     echo "Working";
     
