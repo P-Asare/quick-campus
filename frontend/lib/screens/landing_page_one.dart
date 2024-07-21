55de681bd7d26fbe7c8efbef9dd44a67b097a6e1
P-Asare <pinopalal11@gmail.com>
2024-07-21 14:18:39 +0000

    page: add splash screen page

--- frontend/lib/screens/landing_page_one.dart
+++ frontend/lib/screens/landing_page_one.dart
@@ -1,1 +1,14 @@
-// landing page one
+import 'package:flutter/material.dart';
+
+class FirstLanding extends StatelessWidget {
+  const FirstLanding({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("landing one"),
+      ),
+    );
+  }
+}
--- frontend/lib/screens/splash_screen.dart
+++ frontend/lib/screens/splash_screen.dart
@@ -1,1 +1,19 @@
 // splash screen
+
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF307A59),
+      body: Center(
+        child: Image.asset(
+          "assets/images/quick-campus-splash.png",
+        ),
+      ),
+    );
+  }
+}
