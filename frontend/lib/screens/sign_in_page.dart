8c6024520cb2b8bf3c992095e891a42cb61169af
Palal <86783671+P-Asare@users.noreply.github.com>
2024-07-27 17:54:29 +0000

    Frontend landing pages (#11)
    * dep: add http and provider dependencies
    
    * struct: define file structure for lib folder
    
    * page: add splash screen page
    
    * page: add splash image
    
    * add: first landing page designs
    
    * add: animated route to first page
    
    * add: page transition and landing page two
    
    * add: routing to login page
    
    * update: page routing from landing page

---
+++ frontend/lib/screens/sign_in_page.dart
@@ -0,0 +1,18 @@
+// Login page
+
+import 'package:flutter/material.dart';
+
+class SignInPage extends StatelessWidget {
+  const SignInPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Center(
+          child: Text("Sign in page"),
+        ),
+      ),
+    );
+  }
+}
