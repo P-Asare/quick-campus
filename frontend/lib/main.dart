8d2e0bca99a6fb968c3e164b22d5d5790a1fc912
P-Asare <pinopalal11@gmail.com>
2024-07-29 11:01:00 +0000

    add: design and elements of otp and verify page

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:quickcampus/screens/landing_page_one.dart';
 import 'package:quickcampus/screens/landing_page_two.dart';
+import 'package:quickcampus/screens/otp_page.dart';
 import 'package:quickcampus/screens/sign_in_page.dart';
 import 'package:quickcampus/screens/splash_screen.dart';
+import 'package:quickcampus/screens/verified_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,8 +16,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: SplashScreen(),
-    );
+        debugShowCheckedModeBanner: false, home: VerifiedPage());
   }
 }
---
+++ frontend/lib/screens/verified_page.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:quickcampus/widgets/filled_button.dart';
+
+class VerifiedPage extends StatelessWidget {
+  const VerifiedPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(
+          20,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              "assets/images/confetti_box.png",
+            ),
+
+            // spacing
+            const SizedBox(height: 30),
+            const Text(
+              "Congratulations",
+              style: TextStyle(
+                fontWeight: FontWeight.w800,
+                fontSize: 20,
+              ),
+            ),
+
+            // spacing
+            const SizedBox(height: 20),
+
+            const Text(
+              'Your account has been completed. Go ahead\nand request a delivery.',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.grey,
+              ),
+            ),
+
+            // spacing
+            const SizedBox(height: 35),
+
+            MyFilledButton(
+              title: "Get Started",
+              onPressed: () {},
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
