32f3e7c770d5093c99631d94218dde98a763e69d
P-Asare <pinopalal11@gmail.com>
2024-08-01 18:55:51 +0000

    fix: image picker problem

--- frontend/lib/screens/profile_page.dart
+++ frontend/lib/screens/profile_page.dart
@@ -1,141 +1,127 @@
-// Profile page
-
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:quickcampus/providers/auth_provider.dart';
 import 'package:quickcampus/screens/landing_page_two.dart';
 import 'package:quickcampus/widgets/filled_button.dart';
 
-class ProfilePage extends StatelessWidget {
+class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
 
-  // Log user out of application
+  @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  File? _imageSelected;
+
+  @override
+  void initState() {
+    super.initState();
+    // Ensure that the provider is initialized and has the necessary data.
+  }
+
   void _logOut(BuildContext context) {
     Navigator.of(context)
         .push(MaterialPageRoute(builder: (context) => const SecondLanding()));
   }
 
-  // Forget password
-  void _resetPassword(BuildContext context) {}
+  void _resetPassword(BuildContext context) {
+    // Implementation for resetting the password
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.white,
-        body: Padding(
-          padding: const EdgeInsets.all(20),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const SizedBox(height: 30),
-              Stack(
-                children: [
-                  const CircleAvatar(
-                    radius: 80,
-                    backgroundImage: AssetImage('assets/images/car.png'),
-                  ),
-
-                  // Edit icon
-                  Positioned(
-                    bottom: 0,
-                    right: 0,
-                    child: Container(
-                      padding: const EdgeInsets.all(8),
-                      decoration: const BoxDecoration(
-                        color: Color(0xFF307A59),
-                        shape: BoxShape.circle,
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Consumer<AuthProvider>(
+          builder: (context, authProvider, child) {
+            final user = authProvider.user;
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const SizedBox(height: 30),
+                Stack(
+                  children: [
+                    CircleAvatar(
+                      radius: 80,
+                      backgroundImage: _imageSelected != null
+                          ? FileImage(_imageSelected!)
+                          : const AssetImage("assets/images/no_profile.png")
+                              as ImageProvider,
+                    ),
+                    Positioned(
+                      bottom: 0,
+                      right: 0,
+                      child: Container(
+                        padding: const EdgeInsets.all(3),
+                        decoration: const BoxDecoration(
+                          color: Color(0xFF307A59),
+                          shape: BoxShape.circle,
+                        ),
+                        child: IconButton(
+                          onPressed: () {},
+                          icon: const Icon(Icons.edit, color: Colors.white),
+                        ),
                       ),
-                      child: const Icon(
-                        Icons.edit,
-                        color: Colors.white,
-                      ),
                     ),
-                  )
-                ],
-              ),
+                  ],
+                ),
+                const SizedBox(height: 20),
+                _buildInfoTile(user?.firstName ?? "Unknown"),
+                _buildInfoTile(user?.lastName ?? "Unknown"),
+                _buildInfoTile(user?.email ?? "Unknown"),
+                _buildResetPasswordTile(context),
+                MyFilledButton(
+                  title: "Log Out",
+                  onPressed: () => _logOut(context),
+                ),
+              ],
+            );
+          },
+        ),
+      ),
+    );
+  }
 
-              const SizedBox(height: 20),
+  Widget _buildInfoTile(String value) {
+    return Container(
+      width: double.infinity,
+      padding: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: const Color(0xFFD1E2DB),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: ListTile(
+        title: Text(
+          value,
+          style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w400),
+        ),
+      ),
+    );
+  }
 
-              Container(
-                width: double.infinity,
-                padding: const EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                  color: const Color(0xFFD1E2DB),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: const ListTile(
-                  title: Text(
-                    "Palal",
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.w400,
-                    ),
-                  ),
-                ),
-              ),
-
-              Container(
-                width: double.infinity,
-                padding: const EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                  color: const Color(0xFFD1E2DB),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: const ListTile(
-                  title: Text(
-                    "Asare",
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.w400,
-                    ),
-                  ),
-                ),
-              ),
-
-              Container(
-                width: double.infinity,
-                padding: const EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                  color: const Color(0xFFD1E2DB),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: const ListTile(
-                  title: Text(
-                    "johndoe@ashesi.edu.gh",
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.w400,
-                    ),
-                  ),
-                ),
-              ),
-
-              // password tab
-              Container(
-                width: double.infinity,
-                padding: const EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                  color: const Color(0xFFD1E2DB),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: ListTile(
-                  title: const Text(
-                    "Reset Password?",
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.w400,
-                    ),
-                  ),
-                  trailing: IconButton(
-                    icon: const Icon(Icons.arrow_forward_ios_sharp),
-                    onPressed: () => _resetPassword(context),
-                  ),
-                ),
-              ),
-
-              MyFilledButton(
-                  title: "Log Out", onPressed: () => _logOut(context))
-            ],
-          ),
-        ));
+  Widget _buildResetPasswordTile(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      padding: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: const Color(0xFFD1E2DB),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: ListTile(
+        title: const Text(
+          "Reset Password?",
+          style: TextStyle(fontSize: 18, fontWeight: FontWeight.w400),
+        ),
+        trailing: IconButton(
+          icon: const Icon(Icons.arrow_forward_ios_sharp),
+          onPressed: () => _resetPassword(context),
+        ),
+      ),
+    );
   }
 }
